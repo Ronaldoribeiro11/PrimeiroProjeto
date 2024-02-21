aa2e6de6f7b8fbad37b466f56663884fd90271dc
cj3016544 <cj3016544@LAB01-PC19>
2024-02-21 10:44:51 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+using System.Dynamic;
+
+string nome = "Ronaldo";
+int idade = 18;
+string cidade = string.Empty;
+
+Console.WriteLine("Digite seu nome");
+string entrada = Console.ReadLine();
+if (entrada == nome) ;
+{
+    Console.WriteLine("Ola ronaldo quantos anos voce tem");
+    int insert = int.Parse(Console.ReadLine());
+    if (insert == idade) ;
+    {
+        Console.WriteLine("Muito bem daremos sequencia, de qual cidade vc é");
+        string city = Console.ReadLine();
+
+        if (cidade == city) ;
+        {
+            Console.WriteLine("muito bem cadastro concluido");
+        }
+    }
+
+}
+
+
+    
+
+
+
